5d8d691a8a4391f7d00e43a6799cab76577ba178
cassia11 <cassiagomes0102@gmail.com>
2019-11-13 21:55:18 -0300

    refatora tela de cadastro de pratos

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -2,19 +2,22 @@
     <div class="section">
         <h1 id="title">{{ title }}</h1>
         <div id="box" v-for="plate in plates" :key="plate.id">
-            <p>Nome do prato</p>
-            <label>
-                <input type="text"/>
-            </label>
-            <p>Valor</p>
-            <label>
-                <input type="number"/>
-            </label>
-            <p>Descricao do prato</p>
-            <label>
-                <textarea type="text"></textarea>
-            </label>
+            <label class="mt-20">Nome do Prato</label>
+            <b-input placeholder="Prato" id="input" v-model="form.comments"></b-input>
+            <label class="mt-20">Valor</label>
+            <b-input placeholder="0,00" id="input" v-model="form.value"></b-input>R$
+            <label class="mt-20">Descrição do Prato</label>
+            <b-input placeholder="Insira uma descrição" id="input" v-model="form.description"></b-input>R$
             <p>*A Descricao deve conter até 200 caracteres</p>
+            <b-container fluid>
+                <b-row class="mb-3">
+                    <b-col md="1.5" class="ml-md-auto">
+                        <b-button squared type="submit"
+                                  class="answer-btn mt-20">Salvar
+                        </b-button>
+                    </b-col>
+                </b-row>
+            </b-container>
         </div>
     </div>
 </template>
@@ -22,12 +25,16 @@
 <script>
     import platesService from '../services/plates'
     export default {
-        mounted () {
-            platesService.save()
-                .then((plates) => {
-                    this.$set(this, 'plates', plates)
+        data: () => ({
+            form : {
+                comments: "",
+                value: 0,
+                description:""
+            },
+            submitForm() {
+                platesService.save(this.form)
+            },
                 })
-        }
     }
 </script>
 
