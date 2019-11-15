6554807adcceff276e18473744f61a1f9a05f83f
cassia11 <cassiagomes0102@gmail.com>
2019-11-15 14:12:01 -0200

    adiciona imports, configura axios para comunicacao com BackEnd, adiciona componente base

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
 import Vue from 'vue'
-import BootstrapVue from "bootstrap-vue"
 import App from './App.vue'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap-vue/dist/bootstrap-vue.css"
+import router from './router'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import axios from 'axios'
 
 Vue.use(BootstrapVue);
+Vue.config.productionTip = false
+//axios.defaults.baseURL = (env === 'development') ? 'https://localhost:3000/api/v1' : 'https://shareeat.com/api/v1';
+axios.defaults.baseURL = 'http://localhost:3000/api/v1/'
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  router,
+  template: '<App/>',
+  components: { App }
 });
