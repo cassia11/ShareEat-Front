91f7a242d3fc0d9f1d8a7d57bba31e058628697f
cassia11 <cassiagomes0102@gmail.com>
2019-11-16 19:03:44 -0300

    altera rota de cadastro de novo prato

--- src/services/plates.js
+++ src/services/plates.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export default {
-    save: (plate) => axios.post(`pratos`, plate),
+    save: (plate) => axios.post(`cadastrar`, plate),
     getPlates : (id) => axios.get(`${id}/pratos`)
 }
