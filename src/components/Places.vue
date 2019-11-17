5fe071162dea6487a711626fdd6c3f2bc8d16ba4
cassia11 <cassiagomes0102@gmail.com>
2019-11-17 17:42:53 -0300

    refatora cards no html e adiciona funcao de click

--- src/components/Places.vue
+++ src/components/Places.vue
@@ -1,14 +1,33 @@
 <template>
-  <div class="text-center">
-    <h1 class="row" id="title">{{ title }}</h1>
-    <p class="row" v-if="description == 1">{{ qtd }} lugar cadastrado</p>
-    <p class="row" v-else>{{ description }} lugares cadastrados</p>
-    <div class="row w-50" v-for="place in places" :key="place.id">
-      <div class="col-8">
-        <span class="row text-white">{{ place.name }}</span>
-        <span class="row text-white">{{ place.plates_qtd }} pratos</span>
-      </div>
-      <button class="rounded-circle" href="#">Goo</button>
+  <div>
+    <h1 class="title text-center">
+      <b>{{ title }}</b>
+    </h1>
+    <p class="text-white text-center" v-if="description == 1">{{ qtd }} lugar cadastrado</p>
+    <p class="text-white text-center" v-else>{{ description }} lugares cadastrados</p>
+
+    <div v-for="place in places" :key="place.id">
+      <b-row>
+        <b-col>
+          <div class="box card bg-dark text-white mb-3">
+            <div class="card-body">
+              <span class="row text-white">
+                <b>{{ place.name }}</b>
+              </span>
+              <span class="row text-white">{{ place.plates_qtd }} pratos</span>
+            </div>
+          </div>
+        </b-col>
+
+        <b-col>
+          <b-img
+            @click="more(place.id)"
+            :src="require('../assets/add.png')"
+            class="image"
+            alt="Icone de Adicionar mais pratos"
+          ></b-img>
+        </b-col>
+      </b-row>
     </div>
   </div>
 </template>
@@ -30,26 +49,27 @@ export default {
         this.description = response.data.length;
       })
       .catch(error => {});
+  },
+  methods: {
+    more(identify) {
+      console.log(identify)
+      this.$router.push({ name: 'Plates', paramns: { id: identify}})
+    }
   }
 };
 </script>
 
 <style scoped>
-#title {
+.title {
   font-family: Courier, "Lucida Console", monospace;
-  font-weight: bold;
   font-size: 200%;
   color: #f3aa00;
-  text-align: center;
-}
-p {
-  text-align: center;
-  font-family: "Gill Sans Extrabold", Helvetica, sans-serif;
-  color: #ffffff;
-  opacity: 1;
-  text-align: center;
 }
 .color-card {
   background: #333333 0% 0% no-repeat padding-box;
+}
+.image {
+  width: 20%;
+  height: 80%;
 }
 </style>
