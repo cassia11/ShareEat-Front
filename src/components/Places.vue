98b3d65e171e91a1d6d7b5b8e8c1feb113b55b2f
cassia11 <cassiagomes0102@gmail.com>
2019-11-17 21:05:41 -0300

    botão de adicionar mais pratos

--- src/components/Places.vue
+++ src/components/Places.vue
@@ -6,34 +6,23 @@
     <p class="text-white text-center" v-if="description == 1">{{ qtd }} lugar cadastrado</p>
     <p class="text-white text-center" v-else>{{ description }} lugares cadastrados</p>
 
-    <div class="box card text-white bg-dark mb-3" v-for="place in places" :key="place.id">
-      <div class="card-body" @click="plates(place.id)">
-        <span class="row text-white">
-          <b>{{ place.name }}</b>
-        </span>
-        <span class="row text-white">{{ place.plates_qtd }} pratos</span>
-        <div class="btnMaisBotoes">
-          <div class="col-3 btnMaisBotoesBtn">
-            <button
-              class="btn btn-warning btnCircular btnPrincipal"
-              name="2"
-              @click="more(place.id)"
-            >
-              <i class="fa fa-plus-circle" aria-hidden="true"></i>
-            </button>
+    <div class="div" v-for="place in places" :key="place.id">
+      <div class="box card text-white bg-dark mb-3">
+        <div class="card-body" @click="plates(place.id)">
+          <div class="data">
+            <span class="row text-white">
+              <b>{{ place.name }}</b>
+            </span>
+            <span class="row text-white">{{ place.plates_qtd }} pratos</span>
           </div>
         </div>
       </div>
+      <div class="button">
+        <button right class="btn btn-dark btnCircular" name="2" @click="more(place.id)">
+          <i class="fa fa-plus-circle" aria-hidden="true"></i>
+        </button>
+      </div>
     </div>
-
-    <!-- <b-col>
-          <b-img
-            @click="more(place.id)"
-            :src="require('../assets/add.png')"
-            class="image"
-            alt="Icone de Adicionar mais pratos"
-          ></b-img>
-    </b-col>-->
   </div>
 </template>
 
@@ -80,18 +69,14 @@ export default {
   height: 80%;
 }
 
-.btnMaisBotoes {
-  bottom: 15px;
-  right: 15px;
-  z-index: 100;
-}
 .btnCircular {
+  text-align: right;
   border-radius: 50%;
-}
-
-.btnPrincipal {
   font-size: 20px;
   padding: 15px;
-  text-align: right;
+}
+.div {
+  text-align: right !important;
+  justify-content: space-between;
 }
 </style>
